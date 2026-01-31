38dfe95229aac3e3a552aa510f64eda188995065
Israel <israelmandefro165@gmail.com>
2026-01-31 15:14:53 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -17,11 +17,6 @@ mongoose.connect("mongodb://localhost:27017/mindmate")
 const app = express();
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   console.log(req.method, req.url);
-//   next();
-// });
-
 app.use(chatBot);
 app.use("/mood",mood)
 app.use("/video",video);
